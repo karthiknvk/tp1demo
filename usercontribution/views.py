f7c1e3a267bd142778f39336db85cc4e35d6f6ab
kannoth <kannoth1997house@gmail.com>
2023-12-13 22:02:02 +0530

    demo6

--- usercontribution/views.py
+++ usercontribution/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
+def addreview(request):
+  return HttpResponse("ADD REVIEW")
+
+@login_required
+def addspot(request):
+  return HttpResponse("ADD SPOT")
+
+def showreview(request):
+  pass
